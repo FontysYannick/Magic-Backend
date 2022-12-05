51e11d3fff1d4c25784aaa5b72f8f51cc264a172
Fontys_yannick <y.leentvaar@student.fontys.nl>
2022-12-05 09:45:26 +0100

    fixed git problem and orm does not work

---
+++ Magic/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using Magic_DAL.Context;
+using Magic_Logic.Classes;
+using Magic_Logic.Container;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Magic.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    [EnableCors]
+    public class ProductController : ControllerBase
+    {
+        private Product_Container pc;
+        private readonly IConfiguration configuration;
+        private readonly Magic_Product_Context dbContext;
+
+        public ProductController(Magic_Product_Context dbcontext, IConfiguration ic)
+        {
+            configuration = ic;
+            this.dbContext = dbcontext;
+
+            pc = new(new Product_Context(configuration["db:ConnectionString"]));
+
+        }
+
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public string JsonConverter()
+        {
+            List<Product> products = pc.Getproducts();
+
+            var json = JsonSerializer.Serialize(products);
+            return json;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]orm")]
+        public IActionResult GetProduct()
+        {
+            return Ok(dbContext.Products.ToList());
+        }
+
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult CreateProduct(Product product)
+        {
+            try
+            {
+                pc.Create(product);
+                return Ok(product);
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]")]
+        public IActionResult DeleteProduct(Product product)
+        {
+            try
+            {
+                pc.Delete(product.Id);
+                return Ok(product.Id);
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpPatch]
+        [Route("api/[controller]")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                pc.Update(product);
+                return Ok(product);
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ Magic/Program.cs
@@ -0,0 +1,38 @@
+using Magic.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Magic_Product_Context>(options => options.UseSqlServer("Server=mssqlstud.fhict.local;Database=dbi485841_magic;User Id=dbi485841_magic;Password=Welkom01;"));
+
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("corspolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
